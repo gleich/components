2882b359919dd152108b8a3b264011be20a65761
Matthew Gleich <matthewgleich@gmail.com>
2020-06-22 22:27:00 -0400

    ✨ Initalize naive approche
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
