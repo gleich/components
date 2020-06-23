2e520fcaf341e61a5ab3c1b0b4577f09935f5168
Matthew Gleich <matthewgleich@gmail.com>
2020-06-23 14:15:14 -0400

    ✨ Add ApprovalCard
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,28 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Alex"
-        comment="Super cool post!"
-        time="Today at 2:00AM"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Sam"
-        comment="Wow, super interesting"
-        time="Today at 8:00PM"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        comment="This blog sucks!"
-        time="Yesterday at 4:00PM"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          comment="Super cool post!"
+          time="Today at 2:00AM"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          comment="Wow, super interesting"
+          time="Today at 8:00PM"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          comment="This blog sucks!"
+          time="Yesterday at 4:00PM"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
