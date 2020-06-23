ee6f00ef9bbc118414226c072b16dc7572d194fc
Matthew Gleich <matthewgleich@gmail.com>
2020-06-23 14:01:26 -0400

    🎨 Extract comment into component
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,52 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail
+        author="Alex"
+        comment="Super cool post!"
+        time="Today at 2:00AM"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Sam"
+        comment="Wow, super interesting"
+        time="Today at 8:00PM"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        comment="This blog sucks!"
+        time="Yesterday at 4:00PM"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
